d93bb48adfe1ce5a1303096b478e9d3bea5a18d3
Dovydas Navickas <dovydas@quatrodev.com>
2017-09-12 17:15:44 +0300

    Example with working function extract.

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+class TestFunctionExtract {
+    helloWorld(): string {
+        const greeting: string = "hello";
+        const who: string = "world";
+        return `${greeting} ${who}`;
+    }
+}
